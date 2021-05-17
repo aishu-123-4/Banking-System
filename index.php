4b89ca8064ad17be37f3f7e8400df22051b3256c
aishu-123-4 <67549464+aishu-123-4@users.noreply.github.com>
2021-05-17 18:35:31 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body background="https://img.etimg.com/thumb/msid-77093694,width-1200,height-900,imgsize-125244,resizemode-8,quality-100/industry/banking/finance/banking/fairfax-backed-csb-bank-weighs-buying-out-other-old-private-banks-to-expand-its-balance-sheet.jpg">
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3>Welcome to</h3>
+                  <h1>SPARKS BANK</h1>
+                </div>
+              </div>
+            </div>
+          
+      </div>
+      
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<footer class="text-center mt-5 py-2">
+        <p>&copy 2021. Made by <b>Aiswarya Satish</b></p>
+      </footer>
+  </body>
+</html>
---
+++ sql/bank.sql
@@ -0,0 +1,96 @@
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Aiswarya', 'aiswarya@gmail.com', 33000),
+(2, 'Anshika', 'anshika@gmail.com', 1000),
+(3, 'Amar', 'amar@gmail.com', 88000),
+(4, 'Shreejith', 'sree@gmail.com',700),
+(5, 'Shreya', 'shreya@gmail.com', 2000),
+(6, 'Mohit', 'mohit@gmail.com', 67000),
+(7, 'Rupa', 'rupa@gmail.com', 9000),
+(8, 'Neha', 'neha@gmail.com', 10000),
+(9, 'Gautham', 'gautham@gmail.com', 8900),
+(10, 'Anupuma', 'anu@gmail.com', 34000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
